b7af2564d91449adfb211f54c34dbbfdb979bd72
Anglik <seonwoo6429@gmail.com>
2022-02-11 03:03:12 +0900

    버그 수정

--- Assets/01.Scripts/Manager/SoundManager.cs
+++ Assets/01.Scripts/Manager/SoundManager.cs
@@ -47,6 +47,8 @@ public class SoundManager : MonoBehaviour
         {
             inGameBGM.Stop();
             gameOverBGM.Play();
+
+            playerMoveSound.Stop();
         });
 
         GameManager.Instance.SubBackToMain(() => 
